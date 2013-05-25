89b82dec710018fc0304d9d0d4cc14046f17ca10
xunwenkai <xunwenkai@gmail.com>
2013-05-25 17:46:51 +0800

    2013-05-25 17:46:49

---
+++ kevinlearnphp/1/.svn/text-base/welcome.php.svn-base
@@ -0,0 +1,18 @@
+<html>
+	<body>
+		<!-- get post values from another page 
+		Welcome <?php echo $_POST["name"], " "; ?>
+		You are <?php echo $_POST["age"], "<br>"; ?> 
+		-->
+
+		<!-- get values by get method from another page
+		Welcome <?php echo $_GET["name"], " "; ?>
+		You are <?php echo $_GET["age"], " years old.", "<br>"; ?> 
+		-->
+
+		Welcome <?php echo $_REQUEST["name"], " "; ?>
+		You are <?php echo $_REQUEST["age"], " years old.", "<br>"; ?> 
+
+
+	</body>	
+</html>
--- kevinlearnphp/1/index.php
+++ kevinlearnphp/1/index.php
@@ -3,14 +3,14 @@
 		<p>
 
 		<?php
-		/*	Here introduce the differences between print and echo;
+			/*	Here introduce the differences between print and echo;
 
 			echo "My very first php file.","<br>";
 			print("This line is output by print func");
 			echo "<br>","This line is output by echo and with a change line character";
-		*/
+			*/
 		
-		/*	define var and use var
+			/*	define var and use var
 
 			$myText = "This is my first php file, and i am defining my first var";
 			$myNumber = 53;
@@ -22,13 +22,170 @@
 			print("<br>");
 
 			echo $myText,"<br>",$myNumber,"<br>","output by echo"; 
-		*/
+			*/
 
+			/* concat string and get the position of substring in a string;
 
 			$nameBoy = "xunwenkai";
 			$nameGirl = "luozhaozhao";
+			$conCatNames = $nameBoy." & ".$nameGirl;
 
-			echo($nameBoy." & ".$nameGirl);
+			echo "the length of ", $conCatNames, " is ", strlen($conCatNames),"<br>";
+
+			echo "zhaozhao is the ", strpos($conCatNames, "zhaozhao"), " of ", $conCatNames;
+			*/
+
+			/* concat 2 numbers with x.=y;
+
+			$x = 2;
+			$y = 3;
+			$y.=x;
+			echo $y;
+			*/
+
+			/* test if else 
+
+			$d = date("D");
+			
+			if ($d == "Sat" || $d == "Sun")
+			echo("Have a nice weekend!");
+			else
+				echo "Good good study, day day up.";
+			*/
+		
+			/* switch case break default
+			$x = 1;
+			switch ($x) {
+				case '1':
+					echo "Number 1";
+					break;
+
+				case '2':
+					echo "Number 2";
+					break;
+				
+				default:
+					echo "No number match";
+					break;
+			}
+			*/
+
+			/* Define array with numbers.
+			$names = array("Kevin", "Suzan", "Lucy", "Lily");
+			echo $names[0], " ", $names[1], " ", $names[2], " ", $names[3], "<br>";
+			*/
+
+			/* get array value by relevance quote
+			$ages = array('Kevin' =>28 ,'Suzan' =>26, 'Lucy' =>27, 'Lily' =>25 );
+			echo "Kevin is ", $ages[Kevin], "years old.(this line output by arrayname) ", "<br>";
+			// echo "Kevin is ", $ages[0], "years old.(this line output by arraynumber) ", "<br>";
+			*/
+
+			/* revelance and dimensions array
+			$qvodPlayer = array('Files' => array("Video", "Music", "Pictures"), 
+				'Website' => array("Flag", "Bookmark", "Hotsite"),
+				'Radar' => array("Map", "List"),
+				'Arround' => array("News", "History"));
+
+			echo($qvodPlayer[Files][0]);
+
+			*/
+
+			/* while loop
+			$counter = 1;
+			while ($counter <= 10) {
+				echo "counter comes to ", $counter, "<br>";
+				$counter++;
+			}
+			*/
+
+			/* do...while loop
+			$counter = 0;
+
+			do {
+				$counter++;
+				echo "counter comes to ", $counter, "<br>";
+			} while ($counter < 10);
+			*/
+
+			/* for loop
+
+			for ($i=1; $i<=5; $i++)
+			{
+			  echo "Hello World!<br />";
+			}
+			*/
+
+			/* foreach loop
+
+			$bank = array('Kevin' =>100 ,'Suzan' => 200, 'Lucy' => 150, 'Lily' => -100 );
+
+			foreach ($bank as $value) {
+				echo $bank, " can be borrowed ", $value, " dollors", "<br>";
+			}
+			*/
+
+			/* function with a parameter
+			function writeMyName($name){
+				echo "my name is ", $name, "<br>";
+			}
+
+			echo "What is your name?", "<br>";
+			writeMyName("Kevin");
+			*/
+
+			/* function with return values
+			function getFamilyNames($nameBoy, $nameGirl){
+				$familyName = $nameBoy." ".$nameGirl;
+				return $familyName."<br>";
+			}
+
+			echo(getFamilyNames("Kevin", "Suzan"));
+			echo(getFamilyNames("xunwenkai", "luozhaozhao"));
+			*/
+
+
 		?>
+
+		<!-- a form post to another page
+		<form action="welcome.php" method="post">
+			Name: <input type="text" name="name" />
+			Age:  <input type="text" name="age" />
+				  <input type="submit" />
+		</form> -->
+
+		<!-- a form post to itself
+		<form action="index.php" method="post">
+			Name: <input type="text" name="name" />
+			Age:  <input type="text" name="age" />
+				  <input type="submit" /> -->
+	  	<?php 
+
+	  	/* get user input and verify it in page
+	  		function verifyNameAge($name, $age){
+	  			if ($name == "xunwenkai" && $age == 28) {
+	  				return "pass";
+	  			}
+	  			elseif ($name == "xunwenkai") {
+	  				return "age is wrong";
+	  			}
+	  			elseif ($age == 28) {
+	  				return "name is wrong";
+	  			}
+	  			else 
+	  				return "name and age are wrong";
+	  		}
+	  		echo verifyNameAge($_POST["name"], $_POST["age"]);
+  		*/
+
+
+  		?>
+
+  		<form name="myForm" action="welcome.php" method="get" >
+  			Name:	<input type="text" name="name" />
+			Age:	<input type="text" name="age" />
+					<input type="submit" />
+  		</form>
+  		
 	</body>
 </html>
